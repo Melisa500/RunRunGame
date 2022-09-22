eec3abd4a1b248b0763ac78e6994ad4ee9a44080
Melisa500 <melisabarrera24@gmail.com>
2022-09-22 09:49:17 -0300

    add background, add characters

--- index.js
+++ index.js
@@ -12,7 +12,42 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload (){
-    console.log("H")
+    /* Fondo */
+    this.load.image('background', 'assets/img/background.png');
+    /* Personaje: Jaz */
+    this.load.spritesheet('Jaz', 'assets/img/Jaz-Idle.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+    this.load.spritesheet('Jaz-W', 'assets/img/Jaz-Walk.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+    /* Personaje: Julian */
+    this.load.spritesheet('Julian', 'assets/img/Julian-Idle.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+    this.load.spritesheet('Julian-W', 'assets/img/Julian-Walk.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+    this.load.spritesheet('Julian-S', 'assets/img/Julian-Special.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+    /* Personaje: Luna */
+    this.load.spritesheet('Luna', 'assets/img/Luna-Idle.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+    this.load.spritesheet('Luna-W', 'assets/img/Luna-Walk.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+    /* Personaje: Tito */
+    this.load.spritesheet('Tito', 'assets/img/Julian-Idle.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+    this.load.spritesheet('Tito-W', 'assets/img/Tito-Walk.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
+    this.load.spritesheet('Tito-S', 'assets/img/Tito-Special.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
 }
 
 function create (){
