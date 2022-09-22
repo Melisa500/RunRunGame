5f4820506d542a425098ee6b65eb9c4a28ae1e5d
Melisa500 <melisabarrera24@gmail.com>
2022-09-22 18:09:46 -0300

    the character sprite was adjusted

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
+    height: 400,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: true
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -10,50 +17,97 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var player;
+var platforms;
+var cursors;
+
 
 function preload (){
     /* Fondo */
     this.load.image('background', 'assets/img/background.png');
     /* Personaje: Jaz */
-    this.load.spritesheet('Jaz', 'assets/img/Jaz-Idle.png',
+    this.load.spritesheet('jaz', 'assets/img/jaz-idle.png',
         { frameWidth: 32, frameHeight: 48 }
     );
-    this.load.spritesheet('Jaz-W', 'assets/img/Jaz-Walk.png',
+    this.load.spritesheet('jaz-w', 'assets/img/jaz-walk.png',
         { frameWidth: 32, frameHeight: 48 }
     );
     /* Personaje: Julian */
-    this.load.spritesheet('Julian', 'assets/img/Julian-Idle.png',
+    this.load.spritesheet('julian', 'assets/img/julian-idle.png',
         { frameWidth: 32, frameHeight: 48 }
     );
-    this.load.spritesheet('Julian-W', 'assets/img/Julian-Walk.png',
+    this.load.spritesheet('julian-w', 'assets/img/julian-walk.png',
         { frameWidth: 32, frameHeight: 48 }
     );
-    this.load.spritesheet('Julian-S', 'assets/img/Julian-Special.png',
+    this.load.spritesheet('julian-s', 'assets/img/julian-special.png',
         { frameWidth: 32, frameHeight: 48 }
     );
     /* Personaje: Luna */
-    this.load.spritesheet('Luna', 'assets/img/Luna-Idle.png',
+    this.load.spritesheet('luna', 'assets/img/luna-idle.png',
         { frameWidth: 32, frameHeight: 48 }
     );
-    this.load.spritesheet('Luna-W', 'assets/img/Luna-Walk.png',
+    this.load.spritesheet('luna-w', 'assets/img/luna-walk.png',
         { frameWidth: 32, frameHeight: 48 }
     );
     /* Personaje: Tito */
-    this.load.spritesheet('Tito', 'assets/img/Julian-Idle.png',
+    this.load.spritesheet('tito', 'assets/img/tito-idle.png',
         { frameWidth: 32, frameHeight: 48 }
     );
-    this.load.spritesheet('Tito-W', 'assets/img/Tito-Walk.png',
+    this.load.spritesheet('tito-w', 'assets/img/tito-walk.png',
+            { frameWidth: 49, frameHeight: 48 }
+        );
+    this.load.spritesheet('tito-s', 'assets/img/tito-special.png',
             { frameWidth: 32, frameHeight: 48 }
         );
-    this.load.spritesheet('Tito-S', 'assets/img/Tito-Special.png',
-            { frameWidth: 32, frameHeight: 48 }
-        );
+
+
 }
 
 function create (){
-    console.log("P")
+    this.add.image(400, 300, 'background').setScale(0.7);
+
+    player = this.physics.add.sprite(100, 450, 'tito-w').setScale(3);
+
+    player.setBounce(0.2); /* rebote */
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('tito-w', { start: 0, end: 5 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'tito-w', frame: 1 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('tito-w', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+
 }
 
 function update (){
-    console.log("l")
+    if (cursors.left.isDown){
+        player.setVelocityX(-200);
+        player.anims.play('left', true);
+    }else if (cursors.right.isDown){
+        player.setVelocityX(200);
+        player.anims.play('right', true);
+    }else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+    
+    if (cursors.up.isDown && player.body.touching.down){
+        player.setVelocityY(-330);
+    }
 }
