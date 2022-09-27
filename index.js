7bb71f1198b8e72b1192efd7784ff86b005a0d53
Melisa500 <melisabarrera24@gmail.com>
2022-09-27 17:39:00 -0300

    character setting

--- index.js
+++ index.js
@@ -29,27 +29,24 @@ function preload (){
     this.load.image('ground', 'assets/img/platform.png');
     /* Personaje: Jaz */
     this.load.spritesheet('jaz', 'assets/img/jaz-idle.png',
-        { frameWidth: 32, frameHeight: 48 }
+    { frameWidth: 28, frameHeight: 46, margin: 2, spacing: 19, }
     );
     this.load.spritesheet('jaz-w', 'assets/img/jaz-walk.png',
-        { frameWidth: 32, frameHeight: 48 }
+        { frameWidth: 28, frameHeight: 46, margin: 2, spacing: 19, }
     );
     /* Personaje: Julian */
     this.load.spritesheet('julian', 'assets/img/julian-idle.png',
         { frameWidth: 32, frameHeight: 48 }
     );
     this.load.spritesheet('julian-w', 'assets/img/julian-walk.png',
-        { frameWidth: 32, frameHeight: 48 }
-    );
-    this.load.spritesheet('julian-s', 'assets/img/julian-special.png',
-        { frameWidth: 32, frameHeight: 48 }
+        { frameWidth: 30, frameHeight: 46, margin: 2, spacing: 19,}
     );
     /* Personaje: Luna */
     this.load.spritesheet('luna', 'assets/img/luna-idle.png',
         { frameWidth: 32, frameHeight: 48 }
     );
     this.load.spritesheet('luna-w', 'assets/img/luna-walk.png',
-        { frameWidth: 32, frameHeight: 48 }
+        { frameWidth: 28, frameHeight: 46, margin: 2, spacing: 19,}
     );
     /* Personaje: Tito */
     this.load.spritesheet('tito', 'assets/img/tito-idle.png',
@@ -57,9 +54,6 @@ function preload (){
     );
     this.load.spritesheet('tito-w', 'assets/img/tito-walk.png',
             { frameWidth: 28, frameHeight: 46, margin: 2, spacing: 19,}
-        );
-    this.load.spritesheet('tito-s', 'assets/img/tito-special.png',
-            { frameWidth: 32, frameHeight: 48 }
         );
 
 
@@ -79,30 +73,38 @@ function create (){
     platforms.create(750, 220, 'ground');
     /* end platforms */
 
-    player = this.physics.add.sprite(100, 400, 'tito-w').setScale(3);
 
+    player = this.physics.add.sprite(100, 400, 'jaz-w').setScale(3);
     player.setBounce(0.2); /* rebote */
-    player.setCollideWorldBounds(true);
+    player.setCollideWorldBounds(true); /* respeta los límites de pantalla */
 
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('tito-w', { start: 0, end: 5 }),
+        frames: this.anims.generateFrameNumbers('jaz-w', { start: 0, end: 5 }),
         frameRate: 10,
         repeat: -1,
     });
 
     this.anims.create({
         key: 'turn',
-        frames: [ { key: 'tito-w', frame: 1 } ],
-        frameRate: 20
+        frames: this.anims.generateFrameNumbers('jaz', { start: 0, end: 3 }),
+        //frames: [ { key: 'jaz', frame: 1 } ],
+        frameRate: 10,
+        repeat: -1,
     });
 
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('tito-w', { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('jaz-w', { start: 0, end: 3 }),
         frameRate: 10,
         repeat: -1
     });
+
+ /*    this.anims.create({
+        key: 'jump',
+        frames: [ { key: 'tito-w', frame: 1 } ],
+        frameRate: 20
+    }); */
 
     cursors = this.input.keyboard.createCursorKeys();
     this.physics.add.collider(player, platforms);
@@ -111,32 +113,31 @@ function create (){
 }
 
 function update (){
-    if (cursors.left.isDown){
+    if (cursors.left.isDown ){
         player.setVelocityX(-200);
-        player.anims.play('left', true);
-        if (sentido){
-            player.toggleFlipX()
-            sentido = !sentido
+        if (player.body.touching.down){
+            player.anims.play('left', true);
+            if (sentido){
+                player.toggleFlipX()
+                sentido = !sentido
+            }
         }
-
-
-
     }else if (cursors.right.isDown){
         player.setVelocityX(200);
-        player.anims.play('right', true);
-        if (!sentido){
-            player.toggleFlipX()
-            sentido = !sentido
+        if (player.body.touching.down){
+            player.anims.play('right', true);
+            if (!sentido){
+                player.toggleFlipX()
+                sentido = !sentido
+            }
         }
-
-
-
     }else {
         player.setVelocityX(0);
         player.anims.play('turn');
     }
     
     if (cursors.up.isDown && player.body.touching.down){
-        player.setVelocityY(-330);
+        player.setVelocityY(-350);
+        player.anims.play('turn', true);
     }
 }
